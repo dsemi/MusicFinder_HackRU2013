aa86e54ccf9be96fb81897995c21cc4d4a969ce7
melusom <melusom2@tcnj.edu>
2013-10-13 00:53:37 -0400

    Update URL parse with trackData

--- src/templates/js/helper.js
+++ src/templates/js/helper.js
@@ -1,9 +1,13 @@
 var rdio = require("http://www.rdio.com/api/api.js?client_id=VbZ4G9Z-2iTpsUomxNh68w");
+
+var trackName = {{ trackData["title"] }};
+var artist = {{ trackData["artist"]["name"] }};
+
 rdio.ready(function() {
 	rdio.request({method: "search", content: {
 			type: "track",
 			count: 1,
-			query: trackName
+			query: trackName + ", " + artist
 		},
 		success: function(response) {
 			var top = response.result[0];
