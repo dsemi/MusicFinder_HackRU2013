71a5070a8c5ad30479c72d5a3d96a635cf61d502
melusom <melusom2@tcnj.edu>
2013-10-13 04:01:56 -0400

    Update index.js

--- src/static/index.js
+++ src/static/index.js
@@ -171,27 +171,4 @@ $(document).ready(function() {
 	  copy_info.style.display = 'none';
 	  email_info.style.display = 'none';
 	}
-})	
-
-
-
-
-
-/*
-var rdio = require("http://www.rdio.com/api/api.js?client_id=VbZ4G9Z-2iTpsUomxNh68w");
-rdio.ready(function() {
-	rdio.request({method: "search", content: {
-			type: "track",
-			count: 1,
-			query: trackName
-		},
-		success: function(response) {
-			var top = response.result[0];
-			console.log(top.name + " by " + top.artist);
-		},
-		error: function(response) {
-			console.log("error: " + response.message);
-		}
-	});
-	rdio.player.play({source: top.key});
-});*/
+})
