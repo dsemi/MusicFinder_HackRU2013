5355fb663936fd54a40a630b5bf12d123e3d094f
Mike <melusom2@tcnj.edu>
2013-10-13 00:47:12 -0400

    Revert "Updated URL parsing"
    This reverts commit 78d8ac00070f7cc1d2dd2e9352fbd3e3feea39ef.

--- src/templates/js/helper.js
+++ src/templates/js/helper.js
@@ -1,19 +1,4 @@
 var rdio = require("http://www.rdio.com/api/api.js?client_id=VbZ4G9Z-2iTpsUomxNh68w");
-
-function getQueryVariable() {
-  var query = window.location.search.substring(1);
-  var vars = query.split("&");
-  var newDict = new Object;
-  for (var i=0;i<vars.length;i++) {
-    var pair = vars[i].split("=");
-    var[pair[0]] = pair[1];
-    }
-  } 
-  return newDict;
-}
-
-var trackName = getQueryVariable("trackName");
-
 rdio.ready(function() {
 	rdio.request({method: "search", content: {
 			type: "track",
