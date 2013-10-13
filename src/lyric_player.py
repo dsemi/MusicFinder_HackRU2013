45da6b8b502b85cca7660f1a468db064111380e6
Dan Seminara <semi225599@gmail.com>
2013-10-13 02:02:20 -0400

    moved js files to static

--- src/lyric_player.py
+++ src/lyric_player.py
@@ -10,11 +10,16 @@ app.searcher = LyricSearch()
 
 @app.route('/', methods=['GET','POST'])
 def index():
-    if request.method == 'GET':
-        return render_template('index.html')
-    elif request.method == 'POST':
-        return render_template('helper.html', trackData=search_for_song(request.form.get('lyrics','')))
+    return render_template('helper.html')
+    # if request.method == 'GET':
+    #     return render_template('index.html')
+    # elif request.method == 'POST':
+    #     return render_template('helper.html', trackData=search_for_song(request.form.get('lyrics','')))
 
+
+@app.route('/renderhelper.js')
+def helper():
+    return render_template('js/helper.js', trackData={'title': 'Thunderstruck', 'artist': {'name': 'ACDC'}})
 
 def search_for_song(input):
     return app.searcher.search_by_lyrics(input)
