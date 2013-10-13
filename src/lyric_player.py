dce204702a445b873be8f87ee7dbb93d4f5f975b
Dan Seminara <semi225599@gmail.com>
2013-10-13 02:32:48 -0400

    commit this file so it stop overwriting it

--- src/lyric_player.py
+++ src/lyric_player.py
@@ -10,15 +10,13 @@ app.searcher = LyricSearch()
 
 @app.route('/', methods=['GET','POST'])
 def index():
-    return render_template('helper.html')
+    return render_template('play.html')
     # if request.method == 'GET':
     #     return render_template('index.html')
     # elif request.method == 'POST':
     #     return render_template('helper.html', trackData=search_for_song(request.form.get('lyrics','')))
 
 
-@app.route('/renderhelper.js')
-def helper():
     return render_template('js/helper.js', trackData={'title': 'Thunderstruck', 'artist': {'name': 'ACDC'}})
 
 def search_for_song(input):
