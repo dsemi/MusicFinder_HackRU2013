a168889db56c238e8d35af8efd9c15bb45c8cd43
Dan Seminara <semi225599@gmail.com>
2013-10-13 00:37:33 -0400

    separated into two files

---
+++ src/templates/js/helper.js
@@ -0,0 +1,17 @@
+var rdio = require("http://www.rdio.com/api/api.js?client_id=VbZ4G9Z-2iTpsUomxNh68w");
+rdio.ready(function() {
+	rdio.request({method: "search", content: {
+			type: "track",
+			count: 1,
+			query: trackName
+		},
+		success: function(response) {
+			var top = response.result[0];
+			console.log(top.name + " by " + top.artist);
+		},
+		error: function(response) {
+			console.log("error: " + response.message);
+		}
+	});
+	rdio.player.play({source: top.key});
+});
