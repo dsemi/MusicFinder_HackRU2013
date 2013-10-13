ebbd8713b157177e185c5c693bf072f26f29106f
Dan Seminara <semi225599@gmail.com>
2013-10-13 06:08:36 -0400

    working under flask

--- src/lyric_player.py
+++ src/lyric_player.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python2
 
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, url_for, redirect
 from werkzeug.contrib.fixers import ProxyFix
 from search.lyric_search import LyricSearch
 app = Flask(__name__)
@@ -10,11 +10,16 @@ app.searcher = LyricSearch()
 
 @app.route('/', methods=['GET','POST'])
 def index():
-    return render_template('play.html', trackData={'title': 'Sweet child o mine', 'artist': {'name': 'guns n roses'}})
-    # if request.method == 'GET':
-    #     return render_template('index.html')
-    # elif request.method == 'POST':
-    #     return render_template('helper.html', trackData=search_for_song(request.form.get('lyrics','')))
+    if request.method == 'GET':
+        return render_template('index.html')
+    elif request.method == 'POST':
+        app.current_song = search_for_song(request.args.get('lyrics',''))[0]
+        return "Success"
+
+@app.route('/play.html')
+def play():
+    print app.current_song
+    return render_template('play.html', trackData=app.current_song)
 
 @app.route('/helper.html')
 def helper():
@@ -26,4 +31,4 @@ def search_for_song(input):
 app.wsgi_app = ProxyFix(app.wsgi_app)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0", port=80)
